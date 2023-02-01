23e84b7cea1ac83cde1ed1f222647752dee51bdb
Whycodex <96807353+Whycodex@users.noreply.github.com>
2023-02-02 02:16:02 +0530

    Added solution - LeetHub

--- Count Squares - GFG/count-squares.cpp
+++ Count Squares - GFG/count-squares.cpp
@@ -7,7 +7,7 @@ class Solution {
   public:
     int countSquares(int N) {
         // code here
-        int count=-0;
+        int count=0;
         for(int i=1;i*i<N;i++){
             count++;
         }
